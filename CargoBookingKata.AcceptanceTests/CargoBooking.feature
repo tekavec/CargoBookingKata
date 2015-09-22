1beb9bf49b911ae65d9e46b52c15fdce5a96697c
tekavec <tomaz@codurance.com>
2015-09-22 16:07:20 +0100

    Acceptance tests.

---
+++ CargoBookingKata.AcceptanceTests/CargoBooking.feature
@@ -0,0 +1,17 @@
+﻿Feature: Cargo Bookings
+Background: Cargo and Vessel exist.
+
+@BookCargo
+Scenario: Cargos must be added to Vessels in order to be transported. Vessel should accept cargos if there is capacity.
+	Given a cargo size is 550 cubic feet
+	And a vessel capacity is 9800 cubic feet
+	When the application try to book the cargo on vessel
+	Then the cargo gets the booking number 1
+	And the vessel capacity is 9250 cubic feet.
+
+@RejectCargo
+Scenario: Cargos must be rejected if the Vessel does not have enough capacity.
+	Given a cargo size is 550 cubic feet
+	And a vessel capacity is 120 cubic feet
+	When the application try to book the cargo on vessel
+	Then the cargo is rejected.
---
+++ CargoBookingKata.AcceptanceTests/CargoBookingsSteps.cs
@@ -0,0 +1,50 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace CargoBookingKata.AcceptanceTests
+{
+    [Binding]
+    public class CargoBookingsSteps
+    {
+        private Cargo _cargo;
+        private Vessel _vessel;
+        private readonly CargoBookings _cargoBookings = new CargoBookings();
+
+        [Given(@"a cargo size is (.*) cubic feet")]
+        public void GivenACargoSizeIsCubicFeet(int size)
+        {
+            _cargo = new Cargo(size);
+        }
+        
+        [Given(@"a vessel capacity is (.*) cubic feet")]
+        public void GivenAVesselCapacityIsCubicFeet(int capacity)
+        {
+            _vessel = new Vessel(capacity);
+        }
+        
+        [When(@"the application try to book the cargo on vessel")]
+        public void WhenTheApplicationTryToBookTheCargoOnVessel()
+        {
+            _cargoBookings.Book(_cargo).On(_vessel);
+        }
+        
+        [Then(@"the cargo gets the booking number (.*)")]
+        public void ThenTheCargoGetsTheBookingNumber(int bookingId)
+        {
+            Assert.That(_cargo.BookingId(), Is.EqualTo(bookingId));
+        }
+
+        [Then(@"the vessel capacity is (.*) cubic feet\.")]
+        public void ThenTheVesselCapacityIsCubicFeet_(int capacity)
+        {
+            Assert.That(_vessel.Capacity, Is.EqualTo(capacity));
+        }
+        
+        [Then(@"the cargo is rejected\.")]
+        public void ThenTheCargoIsRejected_()
+        {
+            Assert.That(_cargo.BookingId(), Is.Null);
+        }
+        
+    }
+}
